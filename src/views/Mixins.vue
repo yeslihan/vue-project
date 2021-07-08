0038001922fd47d8c6979f3c597bb22ff4b800e9
DESKTOP-HLLC8OG\yngwie <yeslihan@naver.com>
2021-07-08 15:27:39 +0900

    update

---
+++ src/views/Mixins.vue
@@ -0,0 +1,23 @@
+<template>
+    <div></div>
+</template>
+<script>
+import ApiMixin from '../api.js'
+
+export default {
+    mixins: [ApiMixin],
+    data() {
+        return {
+            productList: []
+        };
+    },
+    async mounted() {
+        this.productList = await this.$callAPI('https://450eb07b-4022-4396-87ee-fc645148f645.mock.pstmn.io/list', 'get');
+        console.log('컴포넌트 mounted');
+        console.log(this.productList);
+    },
+    unmounted() {
+        console.log('컴포넌트 unmounted');
+    }
+}
+</script>
